21cf1b6b4181feb99add6b4b2fbc81d680848a0a
Marcos <juanmarcos156@hotmail.com>
2025-10-08 13:49:22 +0200

    clase cliente

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,9 +1,51 @@
 package cliente;
 
-public class Cliente {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
+public class Cliente {
+	private final int PUERTO = 5000;
+    private final String IP = "127.0.0.1";
+
+    public void iniciar() {
+        Socket cliente = null;
+        ObjectOutputStream salida = null;
+        ObjectInputStream entrada = null;
+        try {
+            cliente = new Socket(IP, PUERTO);//creacion de cliente con IP y PUERTO
+            System.out.println("Conexion realizada con servidor");
+            salida = new ObjectOutputStream(cliente.getOutputStream());
+            entrada = new ObjectInputStream(cliente.getInputStream());
+            String mensaje = (String) entrada.readObject();//lee el servidor
+            System.out.println(mensaje);
+            
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (entrada != null) {
+                    entrada.close();
+                }
+                if (salida != null) {
+                    salida.close();
+                }
+                if (cliente != null) {
+                    cliente.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Fin cliente");
+        }
+    }
+        
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Cliente c = new Cliente();
+        c.iniciar();
 
 	}
 
