23d47b1b0747b5a37d4501fc4e497527abb9644b
Ander719 <ander.arilla99@gmail.com>
2025-10-08 13:31:19 +0200

    Cambios en Cliente y Login_Chat

--- src/cliente/Login_Chat.java
+++ src/cliente/Login_Chat.java
@@ -12,6 +12,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextArea;
 import javax.swing.JCheckBox;
+import java.awt.Color;
+import java.awt.Font;
 
 public class Login_Chat extends JFrame {
 
@@ -42,6 +44,7 @@ public class Login_Chat extends JFrame {
 	 * Create the frame.
 	 */
 	public Login_Chat() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 779, 540);
 		contentPane = new JPanel();
@@ -51,24 +54,27 @@ public class Login_Chat extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblIp = new JLabel("Ip:");
-		lblIp.setBounds(10, 10, 45, 13);
+		lblIp.setBounds(10, 16, 45, 13);
 		contentPane.add(lblIp);
 		
 		textIp = new JTextField();
-		textIp.setBounds(31, 7, 96, 19);
+		textIp.setBounds(51, 13, 96, 19);
 		contentPane.add(textIp);
 		textIp.setColumns(10);
 		
 		textPuerto = new JTextField();
 		textPuerto.setColumns(10);
-		textPuerto.setBounds(208, 7, 96, 19);
+		textPuerto.setBounds(232, 13, 96, 19);
 		contentPane.add(textPuerto);
 		
 		JLabel lblPuerto = new JLabel("Puerto:");
-		lblPuerto.setBounds(142, 13, 45, 13);
+		lblPuerto.setBounds(177, 16, 45, 13);
 		contentPane.add(lblPuerto);
 		
 		JButton btnConectar = new JButton("Conectar");
+		btnConectar.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnConectar.setForeground(new Color(0, 0, 0));
+		btnConectar.setBackground(new Color(128, 255, 128));
 		btnConectar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -77,6 +83,9 @@ public class Login_Chat extends JFrame {
 		contentPane.add(btnConectar);
 		
 		JButton btnDesconetar = new JButton("Desconetar");
+		btnDesconetar.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnDesconetar.setForeground(new Color(0, 0, 0));
+		btnDesconetar.setBackground(new Color(255, 0, 0));
 		btnDesconetar.setEnabled(false);
 		btnDesconetar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -86,7 +95,7 @@ public class Login_Chat extends JFrame {
 		contentPane.add(btnDesconetar);
 		
 		JLabel lblEstado = new JLabel("");
-		lblEstado.setBounds(462, 37, 238, 13);
+		lblEstado.setBounds(517, 37, 238, 13);
 		contentPane.add(lblEstado);
 		
 		JTextArea textAreaChat = new JTextArea();
@@ -98,7 +107,7 @@ public class Login_Chat extends JFrame {
 		contentPane.add(chckbxPrivado);
 		
 		textPara = new JTextField();
-		textPara.setBounds(130, 468, 96, 19);
+		textPara.setBounds(129, 468, 96, 19);
 		contentPane.add(textPara);
 		textPara.setColumns(10);
 		
@@ -112,16 +121,19 @@ public class Login_Chat extends JFrame {
 		textField.setColumns(10);
 		
 		JButton btnEnviar = new JButton("Enviar");
+		btnEnviar.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnEnviar.setForeground(new Color(0, 0, 0));
+		btnEnviar.setBackground(new Color(0, 128, 255));
 		btnEnviar.setBounds(648, 467, 107, 21);
 		contentPane.add(btnEnviar);
 		
 		JLabel lblNombre = new JLabel("Nombre:");
-		lblNombre.setBounds(338, 10, 63, 13);
+		lblNombre.setBounds(338, 16, 63, 13);
 		contentPane.add(lblNombre);
 		
 		JTextField textNombre = new JTextField();
 		textNombre.setColumns(10);
-		textNombre.setBounds(411, 7, 96, 19);
+		textNombre.setBounds(411, 13, 96, 19);
 		contentPane.add(textNombre);
 	}
 }
